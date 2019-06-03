3da709aefcdc6aa97a228f378402841ff806e78d
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-06-03 14:30:49 -0300

    Create ftp_version.py

---
+++ ftp_version.py
@@ -0,0 +1,28 @@
+import requests
+# find and save the current Github release
+html = str(
+    requests.get('https://github.com/containerd/containerd/releases/latest')
+    .content)
+index = html.find('containerd ')
+github_version = html[index + 11:index + 16]
+file = open('github_version.txt', 'w')
+file.writelines(github_version)
+file.close()
+
+# find and save the current Bazel version on FTP server
+html = str(
+    requests.get(
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/containerd/'
+    ).content)
+index = html.rfind('containerd-')
+ftp_version = html[index + 11:index + 16]
+file = open('ftp_version.txt', 'w')
+file.writelines(ftp_version)
+file.close()
+
+# find and save the oldest Bazel version on FTP server
+index = html.find('containerd-')
+delete = html[index + 11:index + 16]
+file = open('delete_version.txt', 'w')
+file.writelines(delete)
+file.close()
