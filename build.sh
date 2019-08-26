52f796bd8b3c6b73599329c909ecc6540c05d31e
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-08-26 16:57:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@ github_version=$ver #$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 
-echo $github_version
-echo $ftp_version 
-
-if $github_version != $ftp_version 
+if [ $github_version != $ftp_version ] 
 then
   echo "Entrou"
   cd $GOPATH/src/github.com
