02c1e9190ab7e103fb12a68382d30e57ec4c43b7
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-09-08 15:51:26 -0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,12 +7,12 @@ if [ $github_version != $ftp_version ]
 then
   git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
   cd repository-scrips/
-  chmod +x empacotar-deb.sh
+  chmod +x empacotar-containerd-deb.sh
   chmod +x empacotar-containerd-rpm.sh
-  sudo mv empacotar-deb.sh $LOCALPATH
+  sudo mv empacotar-containerd-deb.sh $LOCALPATH
   sudo mv empacotar-containerd-rpm.sh $LOCALPATH
   cd $LOCALPATH
-  sudo ./empacotar-deb.sh containerd containerd-$github_version.linux-ppc64le.tar.gz $github_version " "
+  sudo ./empacotar-containerd-deb.sh containerd-$github_version.linux-ppc64le.tar.gz $github_version
   sudo ./empacotar-containerd-rpm.sh containerd-$github_version.linux-ppc64le.tar.gz $github_version
 
   lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/conteinerd/ $LOCALPATH/containerd-$github_version-ppc64le.deb"
