3409f89c2afa92b0189631dd23049b47bec138c2
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2021-01-28 16:52:13 -0300

    Update Ubuntu version, go version and release v1.4.2

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 github_version=$(cat github_version.txt)
+github_version=1.4.2
 ftp_version=$(cat ftp_version.txt)
 LOCALPATH=$GOPATH/src/github.com/containerd/containerd/releases
 
