cf9d10a389e62c264e4e8dd10c4c4a72dccd3ad1
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2022-01-10 17:52:59 -0300

    Update path for ftp_version

--- ftp_version.py
+++ ftp_version.py
@@ -12,7 +12,7 @@ file.close()
 # find and save the current Bazel version on FTP server
 html = str(
     requests.get(
-        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/containerd/latest'
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/containerd'
     ).content)
 index = html.rfind('containerd-')
 ftp_version = html[index + 11:index + 17]
